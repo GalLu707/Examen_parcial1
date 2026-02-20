1cc8c2841b2b6d4df670999942a80bfb7018c2d5
GalLu707 <zasseuwu@gmail.com>
2026-02-20 09:36:10 -0600

    ayuda tengo sueño

--- src/examen_parcial1/MenuPrincipal.java
+++ src/examen_parcial1/MenuPrincipal.java
@@ -4,15 +4,98 @@
  */
 package examen_parcial1;
 
+import java.awt.Color;
+import java.util.ArrayList;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
-public class MenuPrincipal Extends BaseGui{
+
+public class MenuPrincipal extends BaseGui{
+    
+    private JPanel panelPrincipal;
+    private JButton btnAgregarItem, btnRentar, btnEjecutar, btnSalir, btnImprimir;
+    private JLabel lblTitulo;
+    public static ArrayList<RentItem> rentItems = new ArrayList<>();
+    private Game game;
     
     
+    
+   public MenuPrincipal(){
+       super("menu Principal", 615, 550);
+       initComponents();
+   }
    
+   public void initComponents(){
+       panelPrincipal = crearPanelPrincipal();
+       panelPrincipal.setLayout(null);
+       
+       lblTitulo = crearLabelTitulo("menu principal", 175,20, 280,50);
+       panelPrincipal.add(lblTitulo);
+       
+       btnAgregarItem= crearbtn("Agregar Item");
+       btnAgregarItem.setBounds(200, 105, 220, 50);
+       panelPrincipal.add(btnAgregarItem);
+       
+       btnRentar = crearbtn("rentar");
+       btnRentar.setBounds(200,185,220,50);
+       panelPrincipal.add(panelPrincipal);
+       
+       btnEjecutar = crearbtn("Ejecutar Submenú");
+        btnEjecutar.setBounds(200, 265, 220, 50);
+        panelPrincipal.add(btnEjecutar);
+
+        btnImprimir = crearbtn("Ver items registrados");
+        btnImprimir.setBounds(200, 345, 220, 50);
+        panelPrincipal.add(btnImprimir);
+
+        btnSalir = crearbtn("Salir");
+        btnSalir.setBounds(480, 455, 80, 40);
+        btnSalir.setBackground(Color.red);
+        panelPrincipal.add(btnSalir);
+       
+        
+        
+        btnSalir.addActionListener(e-> dispose());
+        btnAgregarItem.addActionListener(e -> agregarItem());
+        btnRentar.addActionListener(e -> Rentar());
+        btnImprimir.addActionListener(e -> imprimirTodo());
+        btnEjecutar.addActionListener(e -> ejecutarSubMenu());
+       
+        
+        setContentPane(panelPrincipal);
+   }
+    
+    private void agregarItem(){
+       
+        
+        
+        
+        
+        
+        
+    }
+    
+    public void Rentar(){
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+    private void imprimirTodo(){
+        
+        
+        
+    }
     
     
-    
-    
+    private void ejecutarSubMenu(){
+        
+    }
     
     
     
